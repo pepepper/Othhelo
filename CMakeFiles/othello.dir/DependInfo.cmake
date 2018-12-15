d1925dd8d0c818d0f00c859d57013738493b9438
pepepper <hollyholly2014@outlook.jp>
2018-12-15 13:20:01 +0900

    ｆｉｘ

---
+++ CMakeFiles/othello.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shirasaka/OneDrive/othellosource/othello/Boards.cpp" "/home/shirasaka/OneDrive/othellosource/othello/CMakeFiles/othello.dir/Boards.cpp.o"
+  "/home/shirasaka/OneDrive/othellosource/othello/Game.cpp" "/home/shirasaka/OneDrive/othellosource/othello/CMakeFiles/othello.dir/Game.cpp.o"
+  "/home/shirasaka/OneDrive/othellosource/othello/Graphic.cpp" "/home/shirasaka/OneDrive/othellosource/othello/CMakeFiles/othello.dir/Graphic.cpp.o"
+  "/home/shirasaka/OneDrive/othellosource/othello/dialogbox.cpp" "/home/shirasaka/OneDrive/othellosource/othello/CMakeFiles/othello.dir/dialogbox.cpp.o"
+  "/home/shirasaka/OneDrive/othellosource/othello/main.cpp" "/home/shirasaka/OneDrive/othellosource/othello/CMakeFiles/othello.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/SDL2"
+  "Boxer/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/shirasaka/OneDrive/othellosource/othello/Boxer/CMakeFiles/Boxer.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/othello.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/othello.dir/Boards.cpp.o"
+  "CMakeFiles/othello.dir/dialogbox.cpp.o"
+  "CMakeFiles/othello.dir/Game.cpp.o"
+  "CMakeFiles/othello.dir/Graphic.cpp.o"
+  "CMakeFiles/othello.dir/main.cpp.o"
+  "othello.pdb"
+  "othello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/othello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
