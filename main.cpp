a907d30e4a2600542792a6f8c550b9b579e6f8d9
pepepper <hollyholly2014@outlook.jp>
2019-03-03 04:14:23 +0900

    net

--- main.cpp
+++ main.cpp
@@ -8,68 +8,58 @@
 #include <typeinfo>
 
 int main(int argc, char *argv[]) {
-	std::string ip, port, user, pass, arg;
+	std::string ip, pass, arg;
 	SDL_Event e;
-	int x, y, mode = -1, netmode;
+	int x, y, mode = -1, netmode = -1;
 	std::unique_ptr<Game> game;
-
+	//部屋番号指定
 	std::cout << "モードを選択してください" << std::endl << "0:オフラインで交互にプレイする 1:オンラインでプレイする場合 :";
-	do {
+	try {
 		std::cin >> arg;
-		if (!arg.compare("0")) {
-			mode = 0;
-			break;
+		mode = std::stoi(arg);
+		if (mode != 0 && mode != 1)throw std::invalid_argument("");
+		if (mode == 1) {
+			std::cout << "モードを選択してください" << std::endl << "0:ホストとして部屋を立てる 1:ゲストとして部屋に入る :";
+			std::cin >> arg;
+			netmode = std::stoi(arg);
+			if (netmode != 0 && netmode != 1)throw std::invalid_argument("");
+			std::cout << "サーバーのIPアドレスまたはドメインを入力してください:" << std::endl;
+			std::cin >> ip;
+			if (netmode == 0) std::cout << "パスワードを設定しますか?" << std::endl << "0:設定しない 1:設定する :";
+			if (netmode == 0) std::cout << "パスワードが設定されていますか?" << std::endl << "0:設定されている 1:設定されていない :";
+			std::cin >> arg;
+			if (!arg.compare("1")) {
+				std::cout << "パスワードを入力してください:" << std::endl;
+				std::cin >> pass;
+			}
 		}
-		if (!arg.compare("1")) {
-			std::cout << "まだ未実装やどぇw" << std::endl;
-		}
-		std::cout << "正しい数値を入力してくだせえ" << std::endl;
-	} while (1);
-	std::cout << "盤面サイズを指定しますか?" << std::endl << "0:指定しない 1:指定する :";
-	do {
-		std::cin >> arg;
-		if (!arg.compare("0")) {
-			game.reset(new Game());
-			break;
-		}
-		if (!arg.compare("1")) {
-			std::string sx, sy;
-			try {
+		if (mode == 0 || netmode == 0) {
+			std::cout << "盤面サイズを指定しますか?" << std::endl << "0:指定しない 1:指定する :";
+			std::cin >> arg;
+			if (!arg.compare("0")) {
+				game.reset(new Game());
+			}
+			else	if (!arg.compare("1")) {
+				std::string sx, sy;
+				int x, y;
+
 				std::cout << "縦のサイズを入力してください:";
 				std::cin >> sy;
-			} catch (const std::invalid_argument& e) {
-				std::cout << "数値ではありません:終了します";
-				return 1;
-			}
-			try {
+				x = std::stoi(sx);
+
 				std::cout << "横のサイズを入力してください:";
 				std::cin >> sx;
-			} catch (const std::invalid_argument& e) {
-				std::cout << "数値ではありません:終了します";
-				return 1;
-			}
+				y = std::stoi(sy);
 
-			game.reset(new Game(std::stoi(sx), std::stoi(sy)));
-			break;
+				game.reset(new Game(x, y));
+			}
+			else { throw std::invalid_argument(""); }
 		}
-		std::cout << "正しい数値を入力してくだせえ" << std::endl;
-	} while (1);
-	/*先に部屋立てた方が先手
-		for (int i = 1; i < argc; i++) {
-			if (!strcmp(argv[i], "-ip"))
-				ip = argv[i += 1];
-			if (!strcmp(argv[i], "-port"))
-				port = argv[i += 1];
-			if (!strcmp(argv[i], "-user"))
-				user = argv[i += 1];
-			if (!strcmp(argv[i], "-h"))
-				netmode = 0;
-			if (!strcmp(argv[i], "-g"))
-				netmode = 1;
-			if (!strcmp(argv[i], "-pass"))
-				pass = argv[i += 1];
-		}
-		*/
+	}
+	catch (const std::invalid_argument& e) {
+		std::cout << "入力が不正です:終了します";
+		return 1;
+	}
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	Graphic graphic;
@@ -82,33 +72,32 @@ int main(int argc, char *argv[]) {
 	while (true) {
 		while (SDL_WaitEvent(&e)) {
 			switch (e.type) {
-				case SDL_WINDOWEVENT:
-					if (e.window.event == SDL_WINDOWEVENT_EXPOSED) {
-						graphic.update();
-					}
-					break;
-				case SDL_MOUSEBUTTONUP:
-					x = (int)(e.button.x / 48) - 4;
-					y = (int)(e.button.y / 48) - 4;
-					if (game->put(x, y)) {
-						graphic.Put(game->board->delta);
-						graphic.changeturn(game->turn);
-						graphic.update();
-					}
-					if (game->full) {
-						dialog.EndGameDialogBox(game->b, game->w, game->howturn);
-						game->full = false;
-					}
-					//if (game->full)dialog.EndGameDialogBox(game->b, game->w, game->howturn);
-					break;
-				case SDL_QUIT:
-					if (dialog.QuitinGameDialogbox()) {
-						game.release();
-						graphic.~Graphic();
-						SDL_Quit();
-						return 0;
-					}
-					break;
+			case SDL_WINDOWEVENT:
+				if (e.window.event == SDL_WINDOWEVENT_EXPOSED) {
+					graphic.update();
+				}
+				break;
+			case SDL_MOUSEBUTTONUP:
+				x = (int)(e.button.x / 48) - 4;
+				y = (int)(e.button.y / 48) - 4;
+				if (game->put(x, y)) {
+					graphic.Put(game->board->delta);
+					graphic.changeturn(game->turn);
+					graphic.update();
+				}
+				if (game->full) {
+					dialog.EndGameDialogBox(game->b, game->w, game->howturn);
+					game->full = false;
+				}
+				break;
+			case SDL_QUIT:
+				if (dialog.QuitinGameDialogbox()) {
+					game.release();
+					graphic.~Graphic();
+					SDL_Quit();
+					return 0;
+				}
+				break;
 			}
 		}
 	}
